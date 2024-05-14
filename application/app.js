9e34c12de219010d51e36c44e20f95c9bc64ea53
AneeshReddyK <Aneeshreddykoppurapu@gmail.com>
2024-05-14 10:36:11 +0530

    first push

---
+++ application/app.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'web_know'
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId);
+});
+
+app.use(express.static('public'));
+app.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    const sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
+    connection.query(sql, [email, password], (err, result) => {
+        if (err) {
+            console.error('Error executing SQL query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        console.log('User registered successfully');
+        res.status(200).send('Registration successful');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
